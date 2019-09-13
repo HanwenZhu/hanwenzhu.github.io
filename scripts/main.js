7400fadebce1a17366a6d0e6cf1ee8f819f3377e
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-09-13 16:54:07 +0800

    Fancy stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,19 @@
+function center(element) {
+    element.css('position', 'absolute');
+    element.css('top', Math.max(0, (($(window).height() - $(element).outerHeight()) / 2) +
+                                                $(window).scrollTop()) + 'px');
+    element.css('left', Math.max(0, (($(window).width() - $(element).outerWidth()) / 2) +
+                                                $(window).scrollLeft()) + 'px');
+    return element;
+}
+
 $(document).ready(() => {
-    alert('hellow rold');
-    setInterval($('div').toggle, 100);
+    setInterval(() => {
+        center($('div'));
+        var random = Math.random();
+        if (random > 0.9) {
+            $('div').hide();
+            setTimeout(() => $('div').show(), random * 100);
+        }
+    }, 100);
 })
